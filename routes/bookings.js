fd60df322b93f39b9bfa026118226927fd62f1a3
Matthew Wilson <matthew.martijn.wilson@gmail.com>
2019-10-15 15:42:42 +0200

    fixes

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router(); 
-const Booking = require('../models/booking');
+const Booking = require('../models/Booking');
 
 router.get('/', (req, res) => {
     res.send("I am inevitable...")
 });
 
 router.post('/', (req, res) => {
+
     console.log(req.body);
-})
+    
+    const booking = new Booking({
+        company: req.body.company,
+        name: req.body.name,
+        receipient: req.body.receipient,
+        location: req.body.location
+    });
+
+    booking.save()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json({ message: err });
+        })
+});
 
 module.exports = router;
