5158fcad8dd2e787d0f41f7d6c288ded9b4fd026
Matthew Wilson <matthew.martijn.wilson@gmail.com>
2019-10-15 11:28:42 +0200

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# takii-bookings
--- public/app.js
+++
@@ -1,8 +0,0 @@
-const firebaseBitches = (length) => {
-
-    let japanIsHere = (length >= 1.5) ? true : false;
-
-    return japanIsHere;
-}
-
-console.log(firebaseBitches(1.69));
