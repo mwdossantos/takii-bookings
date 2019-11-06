a393de9e9755e18563e1391269be0148e15cc363
da-tokkies <timblommesteijn@gmail.com>
2019-11-06 08:59:09 +0100

    Firebase + post
    Everything that works will now also on the timmy branch for developing purposes

---
+++ models/Booking.js
@@ -0,0 +1,21 @@
+//
+
+// const BookingSchema = mongoose.Schema({
+    // company: String,
+    // name: String,   
+    // receipient: String,
+    // createdAtDate: {
+        // type: Date,
+        // default: Date.now
+    // },
+    // location: String, 
+// });
+
+// module.exports = mongoose.model('Booking', BookingSchema);
+
+// {
+// 	"company": "Snap B.V.",
+// 	"name": "Mr. Thanos",
+// 	"receipient": "The Avengers",
+//  "location": "The streets of America baby!"
+// }
