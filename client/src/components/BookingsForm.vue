dca34a49c8510dcf0815ad8390e9eded5d96a3aa
da-tokkies <timblommesteijn@gmail.com>
2019-11-06 16:04:00 +0100

    try to get get to work

--- client/src/components/BookingsForm.vue
+++ client/src/components/BookingsForm.vue
@@ -1,36 +1,25 @@
 <template>
-  <div class="lol">
+  <div>
     <form @submit="formSubmit">
       <h3>Takii Bookings</h3>
       <label for>Company</label>
       <br />
-      <br />
       <input type="text" v-model="company" class="form-control" />
-      <br />
       <br />
       <label for>Name</label>
       <br />
-      <br />
       <input type="text" v-model="name" class="form-control" />
-      <br />
       <br />
       <label for="receipient">receipient</label>
       <br />
-      <br />
       <input type="text" v-model="receipient" class="form-control" />
-      <br />
       <br />
       <label for="location">Location</label>
       <br />
-      <br />
       <input type="text" v-model="location" class="form-control" />
       <br />
-      <br />
-      <button type="submit">Submit</button>
+      <button>Submit</button>
     </form>
-    <br />
-    <br />
-    <div class="divi"></div>
     <article class="appointment-prefix">
       <h4>Company:</h4>
       <p>{{ company }}</p>
@@ -70,9 +59,7 @@ export default {
         })
         .then(function(response) {
           currentObj.output = response.data;
-        }, alert(
-          `You booked a room! It is reserved for ${this.name} from ${this.company} and ${this.receipient} will take you to the ${this.location} room 👩🏻‍🌾`
-        ))
+        })
         .catch(function(error) {
           currentObj.output = error;
         });
@@ -86,48 +73,11 @@ export default {
 body {
   display: flex;
   justify-content: center;
-  align-items: center;
 }
 .appointment-prefix {
   margin-top: 3%;
   width: 20vw;
   border-style: solid;
   border-radius: 4%;
-}
-
-input {
-  padding: 15px 15px;
-  font-size: 18px;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  border-radius: 5px;
-  border: none;
-  outline: none;
-  background-color: rgba(0, 0, 0, 0.05);
-}
-
-button {
-  padding: 10px;
-  font-size: 18px;
-  border-radius: 5px;
-  cursor: pointer;
-  outline: none;
-  margin-top: 20px;
-  transition: all 0.2s ease;
-}
-
-button:active {
-  border: none;
-  background-color: rgba(0, 0, 0, 0.05);
-}
-
-.lol {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.divi {
-  width: 300px;
 }
 </style>
--- routes/getBookings.js
+++ routes/getBookings.js
@@ -1,10 +1,31 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
 const Booking = require('../models/Booking');
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/firestore');
+var db = firebase.firestore();
 
+function getBookings() {
+    let bookings = db.collection('bookings');
+    let query = bookings.get()
+        .then(snapshot => {
+            if (snapshot.empty) {
+                console.log('no documents found');
+                return;
+            }
+
+            snapshot.forEach(doc => {
+                console.log(doc.data());
+                return doc.data();
+            });
+        })
+        .catch(err => {
+            console.log('thanos didnt get the time stone,', err);
+        });
+}
 router.get('/', (req, res) => {
-    
-    res.send("DREAD IT. RUN FROM IT. DESTINY ARRIVES ALL THE SAME. AND NOW IT’S HERE. OR SHOULD I SAY, I AM." +"jew");
+    res.send('destiny arrives' + JSON.stringify(getBookings()));
 });
 
 module.exports = router;
