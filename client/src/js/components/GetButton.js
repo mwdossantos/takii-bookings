092764fa7abbbbbb71461770698778b1445d7a6d
da-tokkies <timblommesteijn@gmail.com>
2019-11-21 13:10:02 +0100

    joink

--- client/src/js/components/GetButton.js
+++ client/src/js/components/GetButton.js
@@ -44,33 +44,26 @@ export default class GetButton extends Component {
         store.state['getState'] = "inactive";
 
         let objects = response;
-
         objects.forEach(booking => {
-
             // create card
+
             const bookingCard = document.createElement('div');
             bookingCard.setAttribute('class', 'card js-preview');
             bookingCard.innerHTML = `<img src="http://1.bp.blogspot.com/-7XiSNsI7Zhc/XL3HYJjFmDI/AAAAAAACt9E/Yzd_cNd6h7IBQcnL4H8R7hHOQXXM_qX6QCK4BGAYYCw/s1600/girassol.gif"
-            class="card-img-top">
-        <div class="card-body">
-            <h5 class="card-title">Your appointment</h5>
-            <p class="card-text">See a preview of your appointment.</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item company"></li>
-            <li class="list-group-item recipient"></li>
-            <li class="list-group-item date"></li>
-            <li class="list-group-item location"></li>
-        </ul>`;
+                class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Your appointment</h5>
+                <p class="card-text">See a preview of your appointment.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item company">${booking.company}</li>
+                <li class="list-group-item recipient">${booking.recipient}</li>
+                <li class="list-group-item date">${booking.date}</li>
+                <li class="list-group-item location">${booking.location}</li>
+            </ul>`;
             document.querySelector('.page').appendChild(bookingCard);
 
-            // add booking data to card
-            document.querySelector('.company').innerHTML = booking.company;
-            document.querySelector('.recipient').innerHTML = booking.recipient;
-            document.querySelector('.date').innerHTML = booking.date;
-            document.querySelector('.location').innerHTML = booking.location;
+            $('#exampleModal').modal('show');
         });
-
-        $('#exampleModal').modal('show')
     }
 }
